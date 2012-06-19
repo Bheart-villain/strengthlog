cbaf12fec3998bd1364240c792be90566b6d3b8c
mike35757@gmail.com <mike35757@gmail.com@98b766d8-7362-08a9-f4b3-4ab172278f08>
2012-06-19 20:16:00 +0000

    init, working clocks, still some issues

--- trunk/strengthlog/src/net/tricky/StrengthlogActivity.java
+++ trunk/strengthlog/src/net/tricky/StrengthlogActivity.java
@@ -1,13 +1,62 @@
 package net.tricky;
 
+import net.tricky.clock.Clock;
+import net.tricky.clock.CountdownTimer;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.widget.TextView;
 
 public class StrengthlogActivity extends Activity {
-    /** Called when the activity is first created. */
+	
+	static final int TIMERDURATION=240;
+	static final int UPDATEDELAY=220;
+	
+	long appStartMillis=0;
+	
+	Clock elapsedTime;
+	CountdownTimer countdownTimer;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        appStartMillis = SystemClock.uptimeMillis();
+        
         setContentView(R.layout.main);
+        TextView mTimeView = (TextView)findViewById(R.id.clock);
+        TextView mCountdownView = (TextView)findViewById(R.id.countdown);
+        
+        elapsedTime = new Clock(UPDATEDELAY,mTimeView);
+        elapsedTime.startClock();
+        
+        countdownTimer = new CountdownTimer(UPDATEDELAY,TIMERDURATION,mCountdownView);
+    }
+    
+    public void buttonGo(){
+    	countdownTimer.startClock();
+    }
+    
+    public void buttonStop(){
+    	countdownTimer.stopClock();
+    }
+    
+    public void buttonResetClock(){
+    	countdownTimer.resetClock();
+    }
+    
+    public void onResume(){
+    	super.onResume();
+    	
+    	if (!elapsedTime.isRunning()){
+    		elapsedTime.startClock();
+    	}
+    }
+    
+    public void onPause(){
+    	super.onPause();
+    	
+    	elapsedTime.stopClock();
+    	countdownTimer.stopClock();
     }
 }
